dd9ffd16b02ed0d9f795d913dd57ae1556ae8912
Richard Kopelow <richardkopelow@gmail.com>
2016-04-14 19:34:57 -0400

    Added exit button to title screen. Esc takes back to title screen

--- Assets/Code/GUI/TitleScreen.cs
+++ Assets/Code/GUI/TitleScreen.cs
@@ -10,9 +10,28 @@ public class TitleScreen : MonoBehaviour
     public GameObject Holder;
     public MissionManager MissionMan;
 
+    Vector3 camStartPos;
+
+    void Start()
+    {
+        camStartPos = CamTransform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            StartCoroutine(leaveGame());
+        }
+    }
+
     public void OnPlayClicked()
     {
         StartCoroutine(enterGame());
+    }
+    public void OnExitClicked()
+    {
+        Application.Quit();
     }
     IEnumerator enterGame()
     {
@@ -28,8 +47,6 @@ public class TitleScreen : MonoBehaviour
         for (int i = 0; i < 50; i++)
         {
             LeftDoor.position = Vector3.Lerp(leftStart, leftEnd, i / 50f);
-            LeftDoor.position = Vector3.Lerp(leftStart, leftEnd, i / 50f);
-            RightDoor.position = Vector3.Lerp(rightStart, rightEnd, i / 50f);
             RightDoor.position = Vector3.Lerp(rightStart, rightEnd, i / 50f);
             yield return null;
         }
@@ -40,5 +57,30 @@ public class TitleScreen : MonoBehaviour
             yield return null;
         }
         MissionMan.SetupMission();
+    }
+    IEnumerator leaveGame()
+    {
+        Vector3 leftStart = LeftDoor.position;
+        Vector3 leftEnd = leftStart - 15 * Vector3.left;
+        Vector3 rightStart = RightDoor.position;
+        Vector3 rightEnd = rightStart - -15 * Vector3.left;
+        Vector3 camStart = CamTransform.position;
+
+        yield return new WaitForSeconds(0.5f);
+        
+        
+        for (int i = 0; i < 50; i++)
+        {
+            CamTransform.position = Vector3.Lerp(CamHome.position,camStartPos, i / 50f);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        for (int i = 0; i < 50; i++)
+        {
+            LeftDoor.position = Vector3.Lerp(leftStart, leftEnd, i / 50f);
+            RightDoor.position = Vector3.Lerp(rightStart, rightEnd, i / 50f);
+            yield return null;
+        }
+        Holder.SetActive(true);
     }
 }
